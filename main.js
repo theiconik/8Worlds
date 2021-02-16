4f39226b327f1ec33adeab28380f007dbe9936e5
theiconik <kumarujjawal51@gmail.com>
2021-02-17 01:00:16 +0530

    Swipr function added

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 //Parallax in Home Section
 let scene = document.getElementById("scene");
 let parallaxInstance = new Parallax(scene);
+
+let keys = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune"
+];
+
+let slider = new Swiper(".swiper-container", {
+  slidesPreView: "auto",
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination: {
+    el: ".planet-links",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<div class="' + className + '">' + keys[index] + "</div>";
+    },
+  },
+});
