cd834ec9d57a056afc601ce0bef198fd7a341ce2
theiconik <kumarujjawal51@gmail.com>
2021-02-18 00:46:20 +0530

    Sliding GSAP animation

--- main.js
+++ main.js
@@ -26,3 +26,55 @@ let slider = new Swiper(".swiper-container", {
     },
   },
 });
+
+// Slider Animation
+slider.on("slideChange",function() {
+  gsap.to(".slide-text span", 0.2, {
+    x: "-100px"
+  })
+  gsap.to(".slide-number span", 0.2, {
+    x: "-100px"
+  })
+  gsap.to(".slide-detail span", 0.2, {
+    x: "-100px"
+  })
+  gsap.to(".slide-facts div", 0.5, {
+    x: "-1000px"
+  })
+  gsap.to(".swiper-slide-active", 0.5, {
+    scale: 0.85
+  })
+  gsap.to(".swiper-slide .slide-img", 1, {
+    rotation: 20
+  })
+})
+
+slider.on("slideChangeTransitionEnd",function() {
+  gsap.to(".slide-text span", 0.2, {
+    x: 0,
+    delay: 0.1
+  })
+  gsap.to(".slide-text span", 0.2, {
+    x: "100px",
+  })
+  gsap.to(".slide-number span", 0.2, {
+    x: 0,
+    delay: 0.1
+  })
+  gsap.to(".slide-number span", 0.2, {
+    x: "100px",
+  })
+  gsap.to(".slide-detail span", 0.2, {
+    x: 0
+  })
+  gsap.to(".slide-facts div", 0.5, {
+    x: 0
+  })
+  gsap.to(".swiper-slide-active", 0.5, {
+    scale: 1,
+    ease: Power4.easeOut
+  })
+  gsap.to(".swiper-slide .slide-img", 1, {
+    rotation: 10
+  })
+})
